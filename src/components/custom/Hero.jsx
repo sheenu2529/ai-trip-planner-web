ba602dd92498f3cc8f8376b3e1b65d4838f6963b
= <=>
2024-09-26 23:31:44 +0530

    updated hero section

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -4,24 +4,21 @@ import { Link } from 'react-router-dom'
 
 function Hero() {
   return (
-    <div className='flex flex-col h-full items-center mx-4 sm:mx-10 md:mx-16 lg:mx-28 xl:mx-56 gap-9'>
-      <h1 className='font-extrabold text-[32px] sm:text-[40px] sm:font-extrabold md:text-[40px] md:font-extrabold text-center mt-24 leading-relaxed'>
-        <span className='text-[#3130FC]'>Discover Your Next Adventure with AI:</span> Personalized itineraries at Your Fingertips
+    <div className='flex flex-col items-center justify-start min-h-screen pt-16 mx-4 sm:mx-10 md:mx-16 lg:mx-28 xl:mx-56 gap-9'>
+      <h1 className='font-extrabold text-[32px] sm:text-[40px] text-center mt-5 leading-relaxed'>
+        <span className='text-[#3130FC]'>Discover Your Next Adventure with AI:</span>
+        <br />
+        Personalized itineraries at Your Fingertips
       </h1>
       <p className='text-base sm:text-lg md:text-xl text-gray-500 text-center leading-relaxed'>
         Your personal trip planner and travel curator, creating custom itineraries tailored to your interests and budget.
       </p>
-
       <Link to='/create-trip'>
         <Button className='px-6 py-3 text-sm sm:text-base'>Get Started, It's Free</Button>
       </Link>
-
-      <img 
-        src='/landing-page.png' 
-        alt='Landing page graphic' 
-      />
+      <img src='/landing-page.png' alt='Landing page graphic' className='w-full h-auto' />
     </div>
   )
 }
 
-export default Hero
+export default Hero;
